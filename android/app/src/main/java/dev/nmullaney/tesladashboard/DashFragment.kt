f4ce70ca9f6f812ef8198dd9c60616011e0ecac2
nmullaney <nora.mullaney@gmail.com>
2021-11-02 22:22:22 -0700

    added circular constraints

--- android/app/src/main/java/dev/nmullaney/tesladashboard/DashFragment.kt
+++ android/app/src/main/java/dev/nmullaney/tesladashboard/DashFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import dev.nmullaney.tesladashboard.databinding.FragmentDashBinding
 
-class DashFragment : Fragment() {
+class teDashFragment : Fragment() {
 
     private lateinit var binding : FragmentDashBinding
 
