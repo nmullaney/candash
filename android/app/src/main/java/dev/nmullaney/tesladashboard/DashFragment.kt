49a536b5da6ba22a31262ad39758f404b1fcb678
Nick Nguyen <osunick@gmail.com>
2021-11-02 22:37:27 -0700

    another typo

--- android/app/src/main/java/dev/nmullaney/tesladashboard/DashFragment.kt
+++ android/app/src/main/java/dev/nmullaney/tesladashboard/DashFragment.kt
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import dev.nmullaney.tesladashboard.databinding.FragmentDashBinding
 
-class teDashFragment : Fragment() {
+class DashFragment : Fragment() {
 
     private lateinit var binding : FragmentDashBinding
 
