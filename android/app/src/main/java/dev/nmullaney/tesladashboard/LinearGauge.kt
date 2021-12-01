126800edd801222519e79ae9839dee8c4559c9d7
Nick Nguyen <osunick@gmail.com>
2021-11-30 22:22:24 -0800

    render regen/power bar from middle

--- android/app/src/main/java/dev/nmullaney/tesladashboard/LinearGauge.kt
+++ android/app/src/main/java/dev/nmullaney/tesladashboard/LinearGauge.kt
@@ -6,38 +6,55 @@ import android.graphics.*
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import kotlin.math.absoluteValue
 
 class LinearGauge @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private val TAG = DashViewModel::class.java.simpleName
-    private val screenWidth : Int = Resources.getSystem().displayMetrics.widthPixels
+    private var windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+    private var realDisplay = windowManager.defaultDisplay
+    private var displayDimensions : Point = Point()
+    private var screenWidth : Int = 100
     private var percentWidth : Float = 0f
     private var isSunUp : Int = 0
     private var lineColor : ColorFilter = PorterDuffColorFilter(getResources().getColor(R.color.dark_gray), PorterDuff.Mode.SRC_ATOP)
     private var renderWidth : Float = 100f
+
     fun getScreenWidth(): Int {
-        return Resources.getSystem().getDisplayMetrics().widthPixels
+        return Resources.getSystem().displayMetrics.widthPixels
     }
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        realDisplay.getRealSize(displayDimensions)
+        screenWidth = displayDimensions.x
         val paint = Paint()
+        var startX : Float = 0f
+        var stopX: Float = 0f
         paint.strokeWidth = 10f
+        paint.setColorFilter(PorterDuffColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_ATOP))
+        canvas?.drawLine(50f, 0f, screenWidth.toFloat() - 50f, 0f, paint)
+
         if (percentWidth < 0f){
             paint.setColorFilter(PorterDuffColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP))
+            startX = (screenWidth / 2f) - renderWidth
+            stopX = screenWidth / 2f
         }else {
+            startX = screenWidth / 2f
+            stopX = (screenWidth / 2f) + renderWidth
             paint.setColorFilter(lineColor)
         }
         Log.d(TAG, "ScreenWidth $screenWidth RenderWidth $renderWidth")
-        canvas?.drawLine(0f, 0f, renderWidth, 0f, paint)
+        canvas?.drawLine(startX, 0f, stopX, 0f, paint)
+        paint.setColorFilter(PorterDuffColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_ATOP))
 
 
     }
     fun setGauge(percent:Float){
         percentWidth = percent
 
-        renderWidth = screenWidth * (percent.absoluteValue)
+        renderWidth = (screenWidth - 100f)/2f * (percent.absoluteValue)
         Log.d(TAG, "percentWidth $screenWidth RenderWidth $renderWidth")
 
     }
