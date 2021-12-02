1f6c3543f4bd6895c38cac86261c3f504df248eb
Nick Nguyen <osunick@gmail.com>
2021-12-01 16:54:47 -0800

    added test for getValueString()

---
+++ android/app/src/test/java/dev/nmullaney/tesladashboard/PandaFrameTest.java
@@ -0,0 +1,11 @@
+package dev.nmullaney.tesladashboard;
+
+import junit.framework.TestCase;
+
+public class PandaFrameTest extends TestCase {
+
+    public void testGetValueString() {
+        String payload = "0110111001000110000111110000000000000000101000000000111100000001";
+        PandaFrame.getValueString(payload, 12, 12);
+    }
+}
