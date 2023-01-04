1afea31c3aa6931c2e74b88053890a6cca17b170
Carlin Hefner <carlinohefner@gmail.com>
2023-01-04 12:27:16 -0500

    fixed info fragment bugs and linted

--- android/app/src/main/java/app/candash/cluster/InfoFragment.kt
+++ android/app/src/main/java/app/candash/cluster/InfoFragment.kt
@@ -3,7 +3,6 @@ package app.candash.cluster
 import android.content.ClipData
 import android.content.Intent
 import android.net.Uri
-import android.net.nsd.NsdServiceInfo
 import android.os.Bundle
 import android.text.SpannableStringBuilder
 import android.util.Log
@@ -17,19 +16,13 @@ import androidx.core.text.buildSpannedString
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import app.candash.cluster.databinding.FragmentInfoBinding
-import kotlinx.coroutines.yield
 import java.io.File
-import java.lang.Thread.sleep
 
 
-class InfoFragment() : Fragment() {
+class InfoFragment : Fragment() {
     private val TAG = InfoFragment::class.java.simpleName
-    private lateinit var binding : FragmentInfoBinding
-    private lateinit var pandaInfo: NsdServiceInfo
+    private lateinit var binding: FragmentInfoBinding
     private lateinit var viewModel: DashViewModel
-
-    private var zeroconfHost : String = ""
-
 
 
     override fun onCreateView(
@@ -55,34 +48,38 @@ class InfoFragment() : Fragment() {
         binding.editIpAddress.text = SpannableStringBuilder(viewModel.serverIpAddress())
 
         binding.saveButton.setOnClickListener {
-            viewModel.saveSettings(getSelectedCANServiceIndex(), binding.editIpAddress.text.toString())
+            viewModel.saveSettings(
+                getSelectedCANServiceIndex(),
+                binding.editIpAddress.text.toString()
+            )
+            reload()
         }
         binding.startButton.setOnClickListener {
-            if (!viewModel.isRunning()){
+            if (!viewModel.isRunning()) {
                 viewModel.startUp()
             }
         }
 
         binding.stopButton.setOnClickListener {
-            if (viewModel.isRunning()){
+            if (viewModel.isRunning()) {
                 viewModel.shutdown()
             }
         }
-        binding.settings.setOnClickListener(){
+        binding.settings.setOnClickListener {
             switchToSettings()
         }
         binding.root.setOnLongClickListener {
             switchToDash()
         }
-        binding.startDashButton.setOnClickListener(){
+        binding.startDashButton.setOnClickListener {
             switchToDash()
         }
 
-        binding.emailLogs.setOnClickListener() {
+        binding.emailLogs.setOnClickListener {
             sendEmailLogs()
         }
 
-        binding.scrollView.setOnLongClickListener{
+        binding.scrollView.setOnLongClickListener {
             switchToDash()
         }
 
@@ -91,10 +88,14 @@ class InfoFragment() : Fragment() {
         }
 
         viewModel.onAllSignals(viewLifecycleOwner) {
-            //logCarState(carState)
             binding.infoText.text = buildSpannedString {
-                val sortedMap = it.toSortedMap()
-                sortedMap.forEach() { entry ->
+                // This might fail when hitting the trashcan button due to a race condition
+                val sortedMap = try {
+                    it.toSortedMap()
+                } catch (exception: ConcurrentModificationException) {
+                    sortedMapOf()
+                }
+                sortedMap.forEach { entry ->
                     bold {
                         append(entry.key)
                         append(": ")
@@ -106,7 +107,7 @@ class InfoFragment() : Fragment() {
         }
     }
 
-    private fun getSelectedCANServiceIndex() : Int {
+    private fun getSelectedCANServiceIndex(): Int {
         return binding.chooseService.selectedItemPosition
     }
 
@@ -130,20 +131,19 @@ class InfoFragment() : Fragment() {
 
     }
 
-    private fun switchToDash() : Boolean {
+    private fun reload(): Boolean {
+        viewModel.switchToInfoFragment()
+        return true
+    }
+
+    private fun switchToDash(): Boolean {
         viewModel.switchToDashFragment()
         return true
     }
 
-    private fun switchToSettings() : Boolean {
+    private fun switchToSettings(): Boolean {
         viewModel.switchToSettingsFragment()
         return true
-    }
-    fun logCarState(carState: CarState) {
-        Log.d(TAG, "Car state size: " + carState.size)
-        carState.forEach {
-            Log.d(TAG, "Name: " + it.key + ", Value: " + it.value)
-        }
     }
 
     private fun sendEmailLogs() {
