59c3d017ff67c8ba457455db2e402d12215839c7
Carlin Hefner <carlinohefner@gmail.com>
2023-02-07 21:40:18 -0500

    Add signal zoom feature in info fragment

--- android/app/src/main/java/app/candash/cluster/InfoFragment.kt
+++ android/app/src/main/java/app/candash/cluster/InfoFragment.kt
@@ -10,6 +10,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.FileProvider.getUriForFile
 import androidx.core.text.bold
 import androidx.core.text.buildSpannedString
@@ -23,6 +25,11 @@ class InfoFragment : Fragment() {
     private val TAG = InfoFragment::class.java.simpleName
     private lateinit var binding: FragmentInfoBinding
     private lateinit var viewModel: DashViewModel
+
+    private val infoTextViews = mutableListOf<View>()
+    private var zoomSignal: String? = null
+    private val signalMaxes = mutableMapOf<String, Float?>()
+    private val signalMinis = mutableMapOf<String, Float?>()
 
 
     override fun onCreateView(
@@ -46,6 +53,11 @@ class InfoFragment : Fragment() {
         binding.chooseService.setSelection(viewModel.getCurrentCANServiceIndex())
 
         binding.editIpAddress.text = SpannableStringBuilder(viewModel.serverIpAddress())
+
+        binding.infoTextConstraint.visibility = View.VISIBLE
+        binding.zoomGauge.visibility = View.INVISIBLE
+        binding.zoomValue.visibility = View.INVISIBLE
+        binding.zoomName.visibility = View.INVISIBLE
 
         binding.saveButton.setOnClickListener {
             viewModel.saveSettings(
@@ -87,23 +99,79 @@ class InfoFragment : Fragment() {
             viewModel.clearCarState()
         }
 
-        viewModel.onAllSignals(viewLifecycleOwner) {
-            binding.infoText.text = buildSpannedString {
-                // This might fail when hitting the trashcan button due to a race condition
-                val sortedMap = try {
-                    it.toSortedMap()
-                } catch (exception: ConcurrentModificationException) {
-                    sortedMapOf()
+        binding.zoomValue.setOnClickListener {
+            zoomSignal = null
+            binding.infoTextConstraint.visibility = View.VISIBLE
+            binding.zoomGauge.visibility = View.INVISIBLE
+            binding.zoomValue.visibility = View.INVISIBLE
+            binding.zoomName.visibility = View.INVISIBLE
+        }
+
+        viewModel.allSignalNames().forEach { signal ->
+            signalMaxes[signal] = null
+            signalMinis[signal] = null
+            val lastViewID = infoTextViews.lastOrNull()?.id ?: binding.headerText.id
+            val textView = TextView(this.context).apply {
+                id = View.generateViewId()
+                layoutParams = ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_PARENT,
+                    ConstraintLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    topToBottom = lastViewID
+                    startToStart = ConstraintLayout.LayoutParams.PARENT_ID
                 }
-                sortedMap.forEach { entry ->
-                    bold {
-                        append(entry.key)
-                        append(": ")
+                setPadding(0, 2, 20, 0)
+                textSize = 18f
+            }
+            textView.setOnClickListener {
+                zoomSignal = signal
+                binding.infoTextConstraint.visibility = View.GONE
+                binding.zoomGauge.visibility = View.VISIBLE
+                binding.zoomValue.visibility = View.VISIBLE
+                binding.zoomName.visibility = View.VISIBLE
+                updateZoom(signal)
+            }
+            viewModel.onSignal(viewLifecycleOwner, signal) { value ->
+                if (value != null) {
+                    val sigMax = signalMaxes[signal]
+                    val sigMin = signalMinis[signal]
+                    if (sigMax == null || value > sigMax) {
+                        signalMaxes[signal] = value
                     }
-                    append(entry.value.toString())
-                    append("\n")
+                    if (sigMin == null || value < sigMin) {
+                        signalMinis[signal] = value
+                    }
+                    textView.text = buildSpannedString {
+                        bold { append("$signal: ") }
+                        append(value.roundToString())
+                    }
+                    textView.visibility = View.VISIBLE
+                } else {
+                    textView.visibility = View.GONE
+                }
+                if (zoomSignal == signal) {
+                    updateZoom(signal)
                 }
             }
+            binding.infoTextConstraint.addView(textView)
+            infoTextViews.add(textView)
+        }
+    }
+
+    private fun updateZoom(signal: String) {
+        val value = viewModel.carState[signal]
+        binding.zoomName.text = signal
+        if (value == null) {
+            binding.zoomGauge.setGauge(0f, 6f)
+            binding.zoomValue.text = "null"
+        } else {
+            val percent = if (value < 0) {
+                value / signalMinis[signal]!! * -1
+            } else {
+                value / signalMaxes[signal]!!
+            }
+            binding.zoomGauge.setGauge(percent, 6f)
+            binding.zoomValue.text = value.roundToString()
         }
     }
 
