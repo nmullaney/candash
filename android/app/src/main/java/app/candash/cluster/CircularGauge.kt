58c727d61c893054b2037ef040e8ebbb2436fda4
Carlin Hefner <carlinohefner@gmail.com>
2023-12-07 22:34:43 -0500

    All the hexagons!

--- android/app/src/main/java/app/candash/cluster/CircularGauge.kt
+++ android/app/src/main/java/app/candash/cluster/CircularGauge.kt
@@ -20,13 +20,55 @@ class CircularGauge @JvmOverloads constructor(
     private var lineColor : ColorFilter = PorterDuffColorFilter(getResources().getColor(R.color.dark_gray), PorterDuff.Mode.SRC_ATOP)
     private var backgroundLineColor : ColorFilter = PorterDuffColorFilter(getResources().getColor(R.color.medium_gray), PorterDuff.Mode.SRC_ATOP)
 
+    private val hexAngle = 60.0
+    private val paint = Paint()
+    private val hexPath = Path()
+    private val innerHexPath = Path()
+    private val outerHexPath = Path()
+
     private var powerWidth : Float = 0f
     private var strokeWidthPct : Float = 12f
     private var charging : Boolean = false
 
     private var cyber : Boolean = false
 
+    private fun buildHexPaths() {
+        val radius = Math.min(width, height) / 2f - paint.strokeWidth / 2
+        for (i in 0 until 6) {
+            val x = width / 2 + radius * Math.cos(Math.toRadians((i * hexAngle))).toFloat()
+            val y = height / 2 + radius * Math.sin(Math.toRadians((i * hexAngle))).toFloat()
+            if (i == 0) {
+                hexPath.moveTo(x, y)
+            } else {
+                hexPath.lineTo(x, y)
+            }
+        }
+        hexPath.close()
 
+        val innerRadius = Math.min(width, height) / 2f - paint.strokeWidth
+        for (i in 0 until 6) {
+            val x = width / 2 + innerRadius * Math.cos(Math.toRadians((i * hexAngle))).toFloat()
+            val y = height / 2 + innerRadius * Math.sin(Math.toRadians((i * hexAngle))).toFloat()
+            if (i == 0) {
+                innerHexPath.moveTo(x, y)
+            } else {
+                innerHexPath.lineTo(x, y)
+            }
+        }
+        innerHexPath.close()
+
+        val outerRadius = Math.min(width, height) / 2f
+        for (i in 0 until 6) {
+            val x = width / 2 + outerRadius * Math.cos(Math.toRadians((i * hexAngle))).toFloat()
+            val y = height / 2 + outerRadius * Math.sin(Math.toRadians((i * hexAngle))).toFloat()
+            if (i == 0) {
+                outerHexPath.moveTo(x, y)
+            } else {
+                outerHexPath.lineTo(x, y)
+            }
+        }
+        outerHexPath.close()
+    }
     fun getScreenWidth(): Int {
         var displayMetrics = DisplayMetrics()
         windowManager.defaultDisplay.getMetrics(displayMetrics)
@@ -46,35 +88,34 @@ class CircularGauge @JvmOverloads constructor(
 
 
     override fun onDraw(canvas: Canvas?) {
-        val width = canvas!!.width
-        val height = canvas!!.height
-        val radius = Math.max(width, height)/2
         Log.d(TAG, "width $width height $height")
 
-        var rect  = RectF(0f.px, height.toFloat(), width.toFloat(), 0f.px)
         super.onDraw(canvas)
         screenWidth = getRealScreenWidth()
         // check if split screen
         if (isSplitScreen()){
             screenWidth = getScreenWidth()
         }
-        val paint = Paint()
-        var startX : Float = 0f
-        var stopX: Float = 0f
+
         paint.strokeWidth = strokeWidthPct / 100f * width
         paint.style = Paint.Style.STROKE
-        rect.inset(0f,-paint.strokeWidth/2);
         paint.setColorFilter(backgroundLineColor)
-        canvas.drawArc(
-            0f + paint.strokeWidth / 2,
-            0f + paint.strokeWidth / 2,
-            width - paint.strokeWidth / 2,
-            height - paint.strokeWidth / 2,
-            90f,
-            360f,
-            false,
-            paint
-        )
+
+        if (cyber) {
+            buildHexPaths()
+            canvas?.drawPath(hexPath, paint)
+        } else {
+            canvas?.drawArc(
+                0f + paint.strokeWidth / 2,
+                0f + paint.strokeWidth / 2,
+                width - paint.strokeWidth / 2,
+                height - paint.strokeWidth / 2,
+                90f,
+                360f,
+                false,
+                paint
+            )
+        }
 
         if (powerWidth < 0f) {
             paint.colorFilter = PorterDuffColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP)
@@ -85,25 +126,41 @@ class CircularGauge @JvmOverloads constructor(
             paint.colorFilter = PorterDuffColorFilter(Color.GREEN, PorterDuff.Mode.SRC_ATOP)
         }
         if (cyber) {
-            paint.strokeCap = Paint.Cap.SQUARE
+            paint.strokeCap = Paint.Cap.BUTT
         } else {
             paint.strokeCap = Paint.Cap.ROUND
         }
         if (powerWidth != 0f) {
-            canvas.drawArc(
-                0f + paint.strokeWidth / 2,
-                0f + paint.strokeWidth / 2,
-                width - paint.strokeWidth / 2,
-                height - paint.strokeWidth / 2,
-                90f,
-                powerWidth,
-                false,
-                paint
-            )
+            if (cyber) {
+                // We're cheating a bit and drawing an arc under a hex mask
+                canvas?.save()
+                canvas?.clipPath(outerHexPath)
+                canvas?.clipPath(innerHexPath, Region.Op.DIFFERENCE)
+                paint.strokeWidth = paint.strokeWidth * 3
+                canvas?.drawArc(
+                    0f + paint.strokeWidth / 2,
+                    0f + paint.strokeWidth / 2,
+                    width - paint.strokeWidth / 2,
+                    height - paint.strokeWidth / 2,
+                    90f,
+                    powerWidth,
+                    false,
+                    paint
+                )
+                canvas?.restore()
+            } else {
+                canvas?.drawArc(
+                    0f + paint.strokeWidth / 2,
+                    0f + paint.strokeWidth / 2,
+                    width - paint.strokeWidth / 2,
+                    height - paint.strokeWidth / 2,
+                    90f,
+                    powerWidth,
+                    false,
+                    paint
+                )
+            }
         }
-        //canvas.drawArc(rect, 0f, 360f, true, paint)
-
-
     }
     fun setGauge(percent:Float, sWidthPct:Float = strokeWidthPct, charge:Boolean = false){
         powerWidth = percent * 360f
